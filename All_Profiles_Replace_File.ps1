c726349666e36711cce3fcdbb9f27bb2a4edac38
Carlos Merino <carlos.merino1291@gmail.com>
2022-05-18 18:21:09 -1000

    Add files via upload

---
+++ All_Profiles_Replace_File.ps1
@@ -0,0 +1,33 @@
+﻿<#
+.SYNOPSIS
+	Quick overview iterate through profiles and replace specific file
+.Description
+	This script first copies the file from the network location to the local machine
+	Then it goes through the list of users on the machine looking for the specific path
+	in this case I wanted to look for a specific file in each of the users profile
+	After it goes through the profiles and has the location of each one it then goes to each
+	location and renames the files and then moves the new file into the location
+	Utilizes the environment variables to give correct path
+	Testing the path to iterate on a machine
+	$_.FullName
+	$_.DirectoryName
+.Notes
+	Author          : Carlos Merino
+    Prerequisite    : User needs to be connected to network path
+#>
+
+$networkPath="\\configNEW.json"
+$tempPath= "C:\Install\config.json"
+
+# Copy file locally
+Copy-Item -Path $networkPath -Destination $tempPath -Force
+
+# Go through the user profiles looking for this specific file
+Get-ItemProperty -Path 'C:\Users\*\file_we_want_to_replace_config.json' |
+
+ForEach-Object {
+# For each user profile that has the file rename it to old in case we need to revert
+Rename-Item -Path $_.FullName -NewName "configOLD.json"
+# From our temp path copy to the directories of the users
+Copy-Item -Path $tempPath -Destination $_.DirectoryName
+}
---
+++ Undo_All_Profiles_Replace_File.ps1
@@ -0,0 +1,16 @@
+﻿<#
+.Description
+	Revert the changes we made in All_Profiles_Replace_File.ps1
+#>
+
+# In the first script we renamed the config to OLD in case we
+# needed to revert changes.  We need to identify which profiles
+# have the old files
+Get-ItemProperty -Path 'C:\Users\*\configOLD.json' |
+
+ForEach-Object {
+# For each user profile first delete the new config.json
+Remove-Item -Path 'C:\Users\*\config.json'
+# then use the full path file name and rename it back to the original
+Rename-Item -Path $_.FullName -NewName "config.json" 
+}
